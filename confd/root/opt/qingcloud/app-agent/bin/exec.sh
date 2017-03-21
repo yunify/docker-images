4cc013171e7ea2139f4374d71193f24336dc1006
jolestar <jolestar@gmail.com>
2017-03-21 23:43:34 +0800

    update confd version

--- confd/root/opt/qingcloud/app-agent/bin/exec.sh
+++ confd/root/opt/qingcloud/app-agent/bin/exec.sh
@@ -11,15 +11,17 @@ CMD_LOG=$AGENT_PATH/cmd.log
 APP_LOG=$AGENT_PATH/app.log
 
 CMD_LINE=$(sed '/^\s*$/d' "$CMD_INFO")
+[[ -z $CMD_LINE ]] && exit 0
+
 CMD_ID=$(echo "$CMD_LINE" | cut -d ":" -f 1)
 CMD=$(echo "$CMD_LINE" | cut -d ":" -f 2-)
 
 echo "$(date +"%Y-%m-%d %H:%M:%S") $CMD_ID [executing]: $CMD" >> "$CMD_LOG" 2>&1
 eval "$CMD" >> "$APP_LOG" 2>&1
 if [ $? -ne 0 ]; then
-    echo "$(date +"%Y-%m-%d %H:%M:%S") $CMD_ID [failed]:  $CMD" >> "$CMD_LOG" 2>&1
+    echo "$(date +"%Y-%m-%d %H:%M:%S") $CMD_ID [failed]: $CMD" >> "$CMD_LOG" 2>&1
     exit 1
 fi
 
-echo "$(date +"%Y-%m-%d %H:%M:%S")​ $CMD_ID [successful]: $CMD"  >> "$CMD_LOG" 2>&1
+echo "$(date +"%Y-%m-%d %H:%M:%S") $CMD_ID [successful]: $CMD" >> "$CMD_LOG" 2>&1
 exit 0
---
+++ jdk/push.sh
@@ -0,0 +1,2 @@
+docker push dockerhub.qingcloud.com/qingcloud/jdk8
+docker push dockerhub.qingcloud.com/qingcloud/jdk8:confd-v0.13.7 
