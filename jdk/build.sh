8efa66fce2317dc9f2995d6b05f4fb6a3ba69538
chai2010 <chaishushan@gmail.com>
2018-06-06 16:36:24 +0800

    update confd version to v0.13.12

--- jdk/build.sh
+++ jdk/build.sh
@@ -1,3 +1,3 @@
 docker build -t jdk8 .
 docker tag jdk8 dockerhub.qingcloud.com/qingcloud/jdk8
-docker tag jdk8 dockerhub.qingcloud.com/qingcloud/jdk8:confd-v0.13.7
+docker tag jdk8 dockerhub.qingcloud.com/qingcloud/jdk8:confd-v0.13.12
